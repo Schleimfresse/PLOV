beeeabf1c28ccfcfb87c4aefc1f64e0d19de47cc
Linus Genz <87274892+Schleimfresse@users.noreply.github.com>
2024-07-21 03:02:31 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var parentElements = document.querySelectorAll('.playlist-item');
+const parentElements = document.querySelectorAll('.playlist-item');
 
 // Iterate over each parent element
 parentElements.forEach(function (parentElement) {
@@ -23,5 +23,31 @@ parentElements.forEach(function (parentElement) {
       document.body.removeChild(popup)
     }, 2100)
   });
+
+
+  parentElement.addEventListener("dblclick", function (event) {
+    window.open(parentElement.getAttribute("data-link"), '_blank');
+  })
+
+  let lastTap = 0;
+  const doubleTapDelay = 300;
+
+  parentElement.addEventListener('touchend', function (event) {
+    const currentTime = new Date().getTime();
+    const tapLength = currentTime - lastTap;
+    if (tapLength < doubleTapDelay && tapLength > 0) {
+      window.open(parentElement.getAttribute("data-link"), '_blank');
+    }
+    lastTap = currentTime;
+  });
+
 });
+
+
+tippy('#help', {
+  content: 'Click to copy link, double click to get redirected',
+  arrow: true,
+});
+
+
 
